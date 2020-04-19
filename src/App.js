04245cdec674f64aeaa50ac3590892b3fe29863a
Manuel Alonge <manuel.alonge@gmail.com>
2020-04-19 16:56:53 +0200

    added numero di sale

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Cinema(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{props.nome || "Mezzano"}</div>,
+    <div>{props.numerodiSale}</div>
   );
 }
+
+
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <h1>Cinema</h1>
+      Nome<Cinema name="Mezzano"></Cinema>
+      Numero di sale:   <Cinema numerodiSale="20"></Cinema>
+    </div>
+  }
+}
+
 
 export default App;
